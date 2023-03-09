2213567d5f7cdcf34a338eeba343287ce7ca3b3f
Konstantin <se7en1703@gmail.com>
2023-03-09 14:29:24 +1000

    Программа для выбора наибольшего из двух ЛЮБЫХ чисел

---
+++ Lesson_and_homework/Example002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+string a = Console.ReadLine();
+double adouble = Convert.Todouble32(a);
+Console.WriteLine("Введите второе число ");
+string b = Console.ReadLine();
+double bdouble = Convert.Todouble32(b);
+if (adouble > bdouble) 
+{ Console.Write("Наибольшее число ");
+           Console.Write(adouble);
+}
+else 
+{ Console.Write("Наибольшее число ");
+     Console.WriteLine(bdouble);
+}
