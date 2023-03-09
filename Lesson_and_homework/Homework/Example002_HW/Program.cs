ced1841b50c9d00ac5a36e44af94d3cab8b4592e
Konstantin <se7en1703@gmail.com>
2023-03-09 16:57:52 +1000

    Done Task 2

--- Lesson_and_homework/Homework/Example002_HW/Program.cs
+++ Lesson_and_homework/Homework/Example002_HW/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число ");
+int c = int.Parse(Console.ReadLine()!);
+int d = a;
+if (a == b & a == c) 
+            Console.Write($"Числа {a}, {b}, {c} являются одинаковыми");
+else if (a == b)
+    d = a;
+else if (a < b)
+    d = b;
+if (c > d)
+    d = c;
+Console.WriteLine($"Число {d} является наибольшим");
