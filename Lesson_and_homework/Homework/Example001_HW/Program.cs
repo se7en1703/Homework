dff99a36f2a40020795d562a9fe3faa3fdc753b0
Konstantin <se7en1703@gmail.com>
2023-03-09 15:41:30 +1000

    Done Task_1

--- Lesson_and_homework/Homework/Example001_HW/Program.cs
+++ Lesson_and_homework/Homework/Example001_HW/Program.cs
@@ -3,15 +3,5 @@ Console.WriteLine("Введите первое число ");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число ");
 int b = int.Parse(Console.ReadLine()!);
-if (a > b) 
-{ Console.Write("Наибольшее число ");
-           Console.WriteLine(a);
-           Console.Write("Наименьшее число ");
-           Console.WriteLine(b);
-}
-else 
-{ Console.Write("Наибольшее число ");
-     Console.WriteLine(b);
-     Console.Write("Наименьшее число ");
-     Console.WriteLine(a);
-}
+if (a > b) Console.Write($"Наибольшее число {a}, наименьшее число {b}");       
+else Console.Write($"Наибольшее число {b}, наименьшее число {a}");
