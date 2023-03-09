3b24a6944975d40134e3ec666eacfceef4a631a3
Konstantin <se7en1703@gmail.com>
2023-03-09 17:16:50 +1000

    Done Task_4

---
+++ Lesson_and_homework/Homework/Lesson_1/Example004_HW/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+while (a <= 0)
+{
+    Console.Write("Ваше число меньше единицы!\nВведите число: ");
+    a = int.Parse(Console.ReadLine()!);
+}
+for (int i = 1; i <= a; i++)
+{
+    if (i % 2 == 0)
+     Console.Write($" {i} ");
+}
