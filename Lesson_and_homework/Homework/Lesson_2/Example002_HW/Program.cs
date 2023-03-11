1f3ab035dc8a13532942d6f8fd09b30b4fef02fd
Konstantin <se7en1703@gmail.com>
2023-03-11 20:24:39 +1000

    Done Task_2

---
+++ Lesson_and_homework/Homework/Lesson_2/Example002_HW/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Программа выведет Вам третью цифру любого числа");
+Console.WriteLine("Введите любое трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+string abs = Convert.ToString(a);
+if (a < 0)
+    {
+        if(a > -100)
+            Console.WriteLine("Третьей цифры нет");
+        else if(a > -1000)
+                Console.WriteLine(abs[1]);
+        else
+            {
+                int eAbs = (a % 1000) / 100;
+                Console.WriteLine(abs[3]);
+            }
+    }
+else if (a > 0)
+    {
+        if(a < 100)
+            Console.WriteLine("Третьей цифры нет");
+       else if(a < 1000)
+                Console.WriteLine(abs[0]);
+        else
+            {
+                int eAbs = (a % 1000) / 100;
+                Console.WriteLine(abs[2]);
+            }
+    }
+else
+    Console.WriteLine("Зачем вы ввели 0? ");
