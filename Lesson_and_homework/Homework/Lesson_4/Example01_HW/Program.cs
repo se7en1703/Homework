0a01155fefc7a086ec2c30f79af5ca62ed260777
Konstantin <se7en1703@gmail.com>
2023-03-19 16:45:20 +1000

    Done Task_27

---
+++ Lesson_and_homework/Homework/Lesson_4/Example01_HW/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+string strA = Convert.ToString(a);
+int z = 0;
+for(int i = 0; i < strA.Length; i++)
+    z = z + int.Parse(strA[i].ToString());
+Console.WriteLine(z);
