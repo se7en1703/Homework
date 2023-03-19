05df5edd41f97833914512309bcf876cef2fd58e
Konstantin <se7en1703@gmail.com>
2023-03-19 17:09:28 +1000

    Done Task_29

---
+++ Lesson_and_homework/Homework/Lesson_4/Example03_HW/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите сколько элементов массива необходимо?: ");
+int a = int.Parse(Console.ReadLine()!);
+int[] array = new int[a];
+for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 31);
+Console.WriteLine($"[{string.Join(", ", array)}]");
