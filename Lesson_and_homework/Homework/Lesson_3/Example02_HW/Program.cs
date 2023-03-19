3ecf493da0523a7484792ebadf1f44d1cc6984fb
Konstantin <se7en1703@gmail.com>
2023-03-19 13:48:57 +1000

    Done Task_23

---
+++ Lesson_and_homework/Homework/Lesson_3/Example02_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int i = 1;
+double s = 0;
+string n = "";
+while(i <= a)
+{
+    s = Math.Pow(i, 3);
+    Convert.ToString(s);
+    n = n + s +", ";
+    i++;
+}
+int len = n.Length;
+n = n.Remove(len - 2) + ".";
+Console.WriteLine($"{n}");
