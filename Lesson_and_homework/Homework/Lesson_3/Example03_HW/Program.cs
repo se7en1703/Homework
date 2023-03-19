e8f02c275b553bcf660d83de3a77824de27717fe
Konstantin <se7en1703@gmail.com>
2023-03-19 13:57:37 +1000

    Done Task_19

---
+++ Lesson_and_homework/Homework/Lesson_3/Example03_HW/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+string b = Convert.ToString(a);
+string z = new string(b.Reverse().ToArray());
+if (b == z)
+    Console.WriteLine($"Число {a} - является полиндромом");
+else
+    Console.WriteLine($"Число {a} - не является полиндромом");
