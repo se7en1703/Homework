eb76be57cba99a2ac5fdaf7c06a83de47392eb76
Konstantin <se7en1703@gmail.com>
2023-03-20 14:31:29 +1000

    Done Picking Blueberries

--- Lesson_and_homework/Homework/Lesson_4/Example03_HW/Program.cs
+++ Lesson_and_homework/Homework/Lesson_4/Example03_HW/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите сколько элементов массива необходимо?: ");
-int a = int.Parse(Console.ReadLine()!);
+int a = new Random().Next(1,21);
 int[] array = new int[a];
 for(int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(1, 31);
