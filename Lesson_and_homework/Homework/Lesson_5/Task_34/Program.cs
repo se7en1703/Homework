e952da26b1ef1223024437bbbba8cc55fe1d19f3
Konstantin <se7en1703@gmail.com>
2023-03-26 17:16:48 +1000

    Done Task_34

---
+++ Lesson_and_homework/Homework/Lesson_5/Task_34/Program.cs
@@ -0,0 +1,22 @@
+﻿void Zapol(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+int Schet(int[] array)
+{
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            n++;
+    }
+    return n;
+}
+Console.Clear();
+Console.WriteLine("Сколько элементов в массиве Вы хотите?: ");
+int a = int.Parse(Console.ReadLine()!);
+int[] array = new int[a];
+Zapol(array);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"В Вашем массиве {Schet(array)} четных элементов");
