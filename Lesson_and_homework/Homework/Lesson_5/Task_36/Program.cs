c3981d268fb87f757b3ce7b0cf71260b39c8a777
Konstantin <se7en1703@gmail.com>
2023-03-26 17:19:55 +1000

    Done Task_36

---
+++ Lesson_and_homework/Homework/Lesson_5/Task_36/Program.cs
@@ -0,0 +1,21 @@
+﻿void Zapol(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+int Schet(int[] array)
+{
+    int n = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        n = n + array[i];
+    }
+    return n;
+}
+Console.Clear();
+Console.WriteLine("Сколько элементов в массиве Вы хотите?: ");
+int a = int.Parse(Console.ReadLine()!);
+int[] array = new int[a];
+Zapol(array);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($" Cуммa элементов, стоящих на нечётных позициях равна {Schet(array)}");
