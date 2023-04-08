38c8aa729822a4a04ff77933d4f32f09795bd0f7
Konstantin <se7en1703@gmail.com>
2023-04-08 19:37:40 +1000

    Task_52 Done

--- Lesson_and_homework/Homework/Lesson_7/Task_52/Program.cs
+++ Lesson_and_homework/Homework/Lesson_7/Task_52/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // От 1 до 10
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+
+        Console.WriteLine();
+    }
+}
+void Arifmet(int[,] matrix)
+{
+    double sr = 0;
+    double z = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sr = sr + matrix[i, j];
+            z++;
+        }
+        sr = Math.Round((sr / z), 2);
+        if (j == matrix.GetLength(1) - 1)
+            Console.Write($"{sr}. \t");
+        else
+            Console.Write($"{sr}; \t");
+        sr = 0;
+        z = 0;
+    }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine($"Среднее арифметическое каждого столбца: ");
+Arifmet(matrix);
