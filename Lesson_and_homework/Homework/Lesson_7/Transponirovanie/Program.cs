53e9d8771a35868948a43c0bfa26ba43ed59884b
Konstantin <se7en1703@gmail.com>
2023-04-08 19:48:51 +1000

    Transponirovanie Done

--- Lesson_and_homework/Homework/Lesson_7/Transponirovanie/Program.cs
+++ Lesson_and_homework/Homework/Lesson_7/Transponirovanie/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // От 1 до 10
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+
+        Console.WriteLine();
+    }
+}
+void Transpon(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Transpon(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
