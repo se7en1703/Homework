d3e42c10bfb25059b08de25fe51ce92348678a9f
Konstantin <se7en1703@gmail.com>
2023-04-08 19:06:35 +1000

    Task_50 Done

--- Lesson_and_homework/Homework/Lesson_7/Task_50/Program.cs
+++ Lesson_and_homework/Homework/Lesson_7/Task_50/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // От 1 до 10
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.Write("Введите позицию элемента: ");
+int[] option = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+if (option[0] > size[0] || option[1] > size[1])
+    Console.WriteLine("Такого элемента нет!!!");
+else
+    Console.WriteLine(matrix[option[0], option[1]]);
