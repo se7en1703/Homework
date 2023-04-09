a10fa355081bc0fe1045c18a8cebcac2f5095135
Konstantin <se7en1703@gmail.com>
2023-04-09 19:05:32 +1000

    Task_68 Done

---
+++ Lesson_and_homework/Homework/Lesson_9/Task_68/Program.cs
@@ -0,0 +1,16 @@
+﻿uint f(uint m, uint n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m > 0) && (n == 0))
+        return f(m - 1, 1);
+    else
+        return f(m - 1, f(m, n - 1));
+}
+Console.Clear();
+Console.Write("Введите начальное число: ");
+uint a = uint.Parse(Console.ReadLine()!);
+Console.Write("Введите начальное число: ");
+uint b = uint.Parse(Console.ReadLine()!);
+Console.WriteLine(f(a, b));
