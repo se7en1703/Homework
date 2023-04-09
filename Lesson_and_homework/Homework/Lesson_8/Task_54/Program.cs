f8e9abeacb2af8e896ea4ce467c4fb56613e0df5
Konstantin <se7en1703@gmail.com>
2023-04-09 13:29:31 +1000

    Task_54 Done

---
+++ Lesson_and_homework/Homework/Lesson_8/Task_54/Program.cs
@@ -0,0 +1,44 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // От 1 до 10
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+
+        Console.WriteLine();
+    }
+}
+void SortMatrix(int[,] matrix)
+{
+    int tmp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+                if (matrix[i, j] > matrix[i, k])
+                {
+                    tmp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = tmp;
+                }
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+SortMatrix(matrix);
+Console.WriteLine("\nОтсортированный массив:");
+PrintMatrix(matrix);
