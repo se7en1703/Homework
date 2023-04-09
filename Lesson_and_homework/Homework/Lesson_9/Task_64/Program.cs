128bf11565905af1d43163db7221304b6f07b1c2
Konstantin <se7en1703@gmail.com>
2023-04-09 18:45:14 +1000

    Task_64 Done

---
+++ Lesson_and_homework/Homework/Lesson_9/Task_64/Program.cs
@@ -0,0 +1,10 @@
+﻿string f(int a)
+{
+    if (a == 1)
+        return $"1";
+    return $"{a} " + f(a - 1);
+}
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(a));
