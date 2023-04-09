98be168bc7db87decdb6e3c871ba697191d1a081
Konstantin <se7en1703@gmail.com>
2023-04-09 18:50:25 +1000

    Task_66 Done

---
+++ Lesson_and_homework/Homework/Lesson_9/Task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿int f(int a, int b)
+{
+    if (b == a)
+        return a;
+    return f(a, b - 1) + b;
+}
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите начальное число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(a, b));
